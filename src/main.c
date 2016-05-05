057ad070c054b926a33b02a4468218c584aec28d
Calvin Rose <calsrose@gmail.com>
2016-05-04 21:30:19 -0400

    Add hello world bakdown file.

--- src/main.c
+++ src/main.c
@@ -1,3 +1,12 @@
-
+/*
+ * The main bakdown program. Read bakdown files, and outputs other kinds
+ * of files.
+ */
 #include <stdio.h>
-#include
+#include "bkd_ast.h"
+#include "bkd_parse.c"
+
+int main (int argc, const char ** argv) {
+    printf("Hello, bakdown!\n");
+    return 0;
+}

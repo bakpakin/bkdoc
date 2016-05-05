5e9e1fb79f0c762256156875e087a93f087d6917
Calvin Rose <calsrose@gmail.com>
2016-05-04 22:14:12 -0400

    Add debug printer stub.

---
+++ src/bkd_debug.c
@@ -0,0 +1,6 @@
+#include "bkd_debug.h"
+#include <stdio.h>
+
+void bkd_printast(struct bkd_document * document) {
+
+}
---
+++ src/bkd_debug.h
@@ -0,0 +1,8 @@
+#ifndef BKD_DEBUG_H_
+#define BKD_DEBUG_H_
+
+#include "bkd_ast.h"
+
+void bkd_printast(struct bkd_document * document);
+
+#endif /* end of include guard: BKD_DEBUG_H_ */
